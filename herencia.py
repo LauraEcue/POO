f07b66904e4832c1235a376b99558512a616c479
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-04-28 20:30:02 -0500

    ejercicio

--- herencia.py
+++ herencia.py
@@ -29,9 +29,23 @@ class Empleado(Persona):
         super().__init__(nombre, apellido, email)
         self.cedula = cedula
         self.cargo = cargo
+        self.salario = salario
 
 #probar
 empleado1 = Empleado("Jhon", "Figueroa", "lui@gmail.com", 109766, "administrador", 2000000)
 empleado1.describir()
+
+class Doctor(Persona):
+    def __init__(
+            self, nombre: str, apellido: str, email: str, cargo: str, especialidad: str):
+        super().__init__(nombre, apellido, email)
+        self.cargo = cargo
+        self.especialidad = especialidad
+
+#probar
+doctor1 = Doctor("Juan", "Lopez", "juan@gmail.com", "Medico general", "Anestesiologo")
+doctor1.describir()
+
+
 
 
