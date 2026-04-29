eb37fd89cea4db8d2216ddd0acc338ee694d9090
LauraEcue <lauravalentinacastillo23@gmail.com>
2026-04-28 19:54:22 -0500

    ejercicio

---
+++ herencia.py
@@ -0,0 +1,37 @@
+class Persona:
+    def __init__(self, nombre: str, apeliido: str, email: str ):
+        self.nombre = nombre
+        self.apellido = apeliido
+        self.email = email
+
+    def describir(self) :
+        print(f"{self.nombre} {self.apellido}")
+
+class Profesor(Persona):
+    def __init__(
+            self, nombre: str, apellido: str, email: str, profesion: str, maestria: str ):
+        super().__init__(nombre, apellido, email)
+        self.profesion = profesion
+        self.maestria = maestria
+
+#probar
+profesor1 = Profesor("Luis", "Gutierrez", "luisgutierrez@gmail.com", "Ingeniero agronomo", "Maestria en recursos agricolas")
+profesor1.describir()
+
+profesor2 = Profesor("Julio", "Diaz", "juliodiaz@gmail.com", "Fisico", "Matematicas")
+profesor2.describir()
+
+
+
+class Empleado(Persona):
+    def __init__(
+            self, nombre: str, apellido: str, email: str, cedula: int, cargo: str, salario: int):
+        super().__init__(nombre, apellido, email)
+        self.cedula = cedula
+        self.cargo = cargo
+
+#probar
+empleado1 = Empleado("Jhon", "Figueroa", "lui@gmail.com", 109766, "administrador", 2000000)
+empleado1.describir()
+
+
